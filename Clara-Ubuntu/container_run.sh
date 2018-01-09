f8509a208fd55025eb2a1ce9d632c072dc8a0e4e
Capuccino <chinodesuuu@gmail.com>
2018-01-09 14:15:03 +0000

    fix perm errors for good

--- Clara-Ubuntu/container_run.sh
+++ Clara-Ubuntu/container_run.sh
@@ -3,5 +3,6 @@ export USER_ID=$(id -u)
 export GROUP_ID=$(id -g)
 envsubst < /tmp/passwd_template > /tmp/passwd
 
-/usr/bin/pm2 start /opt/app/bot.js && \
-/usr/bin/pm2 logs bot
+cd /opt/app && \
+/usr/bin/pm2 start /opt/app/pm2.json && \
+/usr/bin/pm2 logs Clara
