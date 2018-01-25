0b746733d9e906fb45d74958e4602cb5f92428e9
Capuccino <chinodesuuu@gmail.com>
2018-01-25 04:12:16 +0000

    fix user perms

--- Clara-Alpine-Nightly/compile_container.sh
+++ Clara-Alpine-Nightly/compile_container.sh
@@ -40,9 +40,9 @@ chmod g+rw /opt
 chgrp root /opt
 
 # allow to run on openshift
-chown -R user:root /opt
 chown -R user:root /opt/app
 chown -R user:root /opt/app/*
+chmod o+x /opt
 chmod -R g+rw /opt/app
 chmod -R g+rw /home/user
 find /home/user -type d -exec chmod g+x {} +
