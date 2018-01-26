cf80c0ae4e13a686bf0f5938e94fb1a15c512d6b
Capuccino <chinodesuuu@gmail.com>
2018-01-26 15:46:51 +0000

    point stuff properly

--- Clara-Alpine/container_run.sh
+++ Clara-Alpine/container_run.sh
@@ -4,5 +4,5 @@ export GROUP_ID=$(id -g)
 envsubst < /tmp/passwd_template > /tmp/passwd
 
 cd /opt/app && \
-/usr/bin/node /opt/app/bot.js \
---harmony 
+/usr/local/bin/pm2 start /opt/app/pm2.json && \
+/usr/local/bin/pm2 logs Clara
