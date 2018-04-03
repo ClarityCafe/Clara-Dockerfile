324a2506b722b48c789c0015b965c21586bfc56c
Capuccino <enra@headbow.stream>
2018-04-03 08:52:23 +0000

    fancy build echos

--- Clara-Ubuntu-Nightly/compile_container.sh
+++ Clara-Ubuntu-Nightly/compile_container.sh
@@ -2,6 +2,9 @@
 
 # The reason we're doing the package install here is to keep everything in one layer for easier downloads
 # It's relatively more convinient this way
+
+printf " >>>>>>>>>>>>>>>> INSTALL DEPS"
+
 apt update && \
 apt install -y \
     apt-utils \
@@ -21,9 +24,11 @@ apt install -y \
     openssh-server \
     gettext  \
 
+printf "<<<<<<<<<<<<<<<<<<<<<<<<<< INSTALL DEPS"
+printf ">>>>>>>>>>>>>>>>>>>>>>>>>> INSTALL NODE"
 curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash - && \
 sudo apt -y install nodejs
-
+printf "<<<<<<<<<<<<<<<<<<<<<<<<<< INSTALL NODE"
 # npm install yo?
 npm i -g pm2  && \
 mkdir /.pm2
@@ -39,19 +44,22 @@ mkdir /.npm
 #   rm -rf /usr/src/Python-3.6.4.tgz && \
 # /usr/bin/python3 -V
 
-
+printf ">>>>>>>>>>>>>>>>>>>>> INSTALL PYTHON3.6"
 #install Python via APT repo instead
 add-apt-repository ppa:jonathonf/python-3.6 && \
 apt update && \
 apt -y install python3.6
-
+printf "<<<<<<<<<<<<<<<<<<<<<  INSTALL PYTHON3.6"
+printf ">>>>>>>>>>>>>>>>>>>>>  CREATE USER"
 # Create user
 mkdir /var/run/sshd && \
 sed 's@session\s*required\s*pam_loginuid.so@session optional pam_loginuid.so@g' -i /etc/pam.d/sshd && \
 echo "%sudo ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers && \
 useradd -u 1000 -G users,sudo -d /home/user --shell /bin/bash -m user && \
 usermod -p "*" user 
+printf "<<<<<<<<<<<<<<<<<<<<<<  CREATE USER"
 
+printf ">>>>>>>>>>>>>>>>>>>>>> INSTALL CLARA"
 #clone repo, expose Clara as app, then trim contents
 cd /opt && \
 git clone https://github.com/ClarityMoe/Clara && \
@@ -65,6 +73,9 @@ cd /opt/app && \
 npm i --save && \
 npm run circle-postinstall
 
+printf "<<<<<<<<<<<<<<<<<<<<<<<< INSTALL CLARA"
+
+printf ">>>>>>>>>>>>>>>>>>>>>>>> OPENSHIFT OVERRIDES"
 # perm root awau
 chmod g+rw /opt
 chgrp root /opt
@@ -78,3 +89,20 @@ chmod -R g+rw /.npm
 chmod -R g+rw /opt/app
 chmod -R g+rw /home/user
 find /home/user -type d -exec chmod g+x {} +
+echo 'done'
+
+printf "<<<<<<<<<<<<<<<<<<<<<<<<< OPENSHIFT OVERRIDES"
+# cleanup
+printf ">>>>>>>>>>>>>>>>>>>>>>>> CLEANUP"
+apt -y remove \
+    apt-utils \
+    zlib1g-dev \
+    build-essential \
+    software-properties-common \
+    python-software-properties \
+    curl \
+    wget \
+    cmake \
+    openssh-server \
+    gettext;
+printf "<<<<<<<<<<<<<<<<<<<<<<<< CLEANUP"
--- Clara-Ubuntu/compile_container.sh
+++ Clara-Ubuntu/compile_container.sh
@@ -2,6 +2,9 @@
 
 # The reason we're doing the package install here is to keep everything in one layer for easier downloads
 # It's relatively more convinient this way
+
+printf " >>>>>>>>>>>>>>>> INSTALL DEPS"
+
 apt update && \
 apt install -y \
     apt-utils \
@@ -21,9 +24,11 @@ apt install -y \
     openssh-server \
     gettext  \
 
+printf "<<<<<<<<<<<<<<<<<<<<<<<<<< INSTALL DEPS"
+printf ">>>>>>>>>>>>>>>>>>>>>>>>>> INSTALL NODE"
 curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash - && \
 sudo apt -y install nodejs
-
+printf "<<<<<<<<<<<<<<<<<<<<<<<<<< INSTALL NODE"
 # npm install yo?
 npm i -g pm2  && \
 mkdir /.pm2
@@ -39,30 +44,38 @@ mkdir /.npm
 #   rm -rf /usr/src/Python-3.6.4.tgz && \
 # /usr/bin/python3 -V
 
-
+printf ">>>>>>>>>>>>>>>>>>>>> INSTALL PYTHON3.6"
 #install Python via APT repo instead
 add-apt-repository ppa:jonathonf/python-3.6 && \
 apt update && \
 apt -y install python3.6
-
+printf "<<<<<<<<<<<<<<<<<<<<<  INSTALL PYTHON3.6"
+printf ">>>>>>>>>>>>>>>>>>>>>  CREATE USER"
 # Create user
 mkdir /var/run/sshd && \
 sed 's@session\s*required\s*pam_loginuid.so@session optional pam_loginuid.so@g' -i /etc/pam.d/sshd && \
 echo "%sudo ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers && \
 useradd -u 1000 -G users,sudo -d /home/user --shell /bin/bash -m user && \
 usermod -p "*" user 
+printf "<<<<<<<<<<<<<<<<<<<<<<  CREATE USER"
 
+printf ">>>>>>>>>>>>>>>>>>>>>> INSTALL CLARA"
 #clone repo, expose Clara as app, then trim contents
 cd /opt && \
 git clone https://github.com/ClarityMoe/Clara && \
 cd Clara && \
+git checkout development && \
 mkdir /opt/app && \
 mv /opt/Clara/src/* /opt/app && \
 mv /opt/Clara/package.json /opt/app && \
 rm -rf /opt/Clara && \
 cd /opt/app && \
-npm i --save 
+npm i --save && \
+npm run circle-postinstall
 
+printf "<<<<<<<<<<<<<<<<<<<<<<<< INSTALL CLARA"
+
+printf ">>>>>>>>>>>>>>>>>>>>>>>> OPENSHIFT OVERRIDES"
 # perm root awau
 chmod g+rw /opt
 chgrp root /opt
@@ -76,9 +89,11 @@ chmod -R g+rw /.npm
 chmod -R g+rw /opt/app
 chmod -R g+rw /home/user
 find /home/user -type d -exec chmod g+x {} +
+echo 'done'
 
+printf "<<<<<<<<<<<<<<<<<<<<<<<<< OPENSHIFT OVERRIDES"
 # cleanup
-
+printf ">>>>>>>>>>>>>>>>>>>>>>>> CLEANUP"
 apt -y remove \
     apt-utils \
     zlib1g-dev \
@@ -90,3 +105,4 @@ apt -y remove \
     cmake \
     openssh-server \
     gettext;
+printf "<<<<<<<<<<<<<<<<<<<<<<<< CLEANUP"
